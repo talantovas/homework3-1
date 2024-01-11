76fe507ddeb8e5289dc0145554471216ba39fd07
talantovas <stalantovva@gmail.com>
2024-01-11 15:59:10 +0600

    Initial commit

---
+++ app/src/main/java/com/geeks/myapplication/MainActivity.java
@@ -0,0 +1,36 @@
+package com.geeks.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText editText;
+    private EditText editText2;
+    private EditText editText3;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        editText = findViewById(R.id.editText);
+        editText2 = findViewById(R.id.editText2);
+        editText3 = findViewById(R.id.editText3);
+    }
+
+    public void buttonSend(View view) {
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto:"));
+        intent.putExtra(Intent.EXTRA_EMAIL, new String[]{editText.getText().toString()});
+        intent.putExtra(Intent.EXTRA_SUBJECT, editText2.getText().toString());
+        intent.putExtra(Intent.EXTRA_TEXT, editText3.getText().toString());
+        startActivity(intent);
+    }
+
+
+}
